a94a399f1fb01a9ae6b19d9dc0f64c0d4b6d49ca
Milad Rasouli <miladrasooli021@gmail.com>
2024-04-08 18:37:11 +0330

    modal added

--- js/app.js
+++ js/app.js
@@ -8,6 +8,11 @@ new Vue({
     el: '#my-app',
     data: {
         contacts: [],
+        addModal: false,
+        name: '',
+        family: '',
+        phone: '',
+        email: '',
     },
     methods: {
         GetData() {
@@ -16,14 +21,45 @@ new Vue({
 
                     // console.log(response.data);
                     this.contacts = response.data;
-                    console.log(contacts);
+                    console.log(this.contacts);
                 })
                 .catch(error => {
                     console.log("ERROR : " + error)
                 });
+        },
+        addNew() {
+            let https = "https://localhost:7086/api/Contacts";
+
+            axios.post(https, {
+                name: this.name,
+                family: this.family,
+                phone: this.phone,
+                email: this.email,
+            })
+                .then((response) => {
+                    this.GetData();
+                    this.addModal = false;
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+
+
         }
     },
     mounted() {
         this.GetData();
     },
+
+    computed: {
+        isEmpty() {
+            if (!this.name || !this.family || !this.email || !this.phone) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
 });
